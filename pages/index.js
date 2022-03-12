f7eccfde2639ece56e2ea329e604e7dca86c4242
troels_a <troels@blck.dk>
2022-03-12 13:40:44 +0100

    🤷‍♂️

--- pages/index.js
+++ pages/index.js
@@ -30,3 +30,10 @@ export default function Home(props){
         <img src='/img/logo.png'/>
     </Wrapper>
 }
+
+export async function getStaticProps(){
+    
+    return {
+        props: {}
+    }
+}
