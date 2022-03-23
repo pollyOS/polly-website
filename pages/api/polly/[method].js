f0191115be70ea0aa5a5c87721db12fbd8bf9ad2
Troels Abrahamsen <troels@blck.dk>
2022-03-23 17:07:36 +0100

    api mehtod result parsing

--- pages/api/polly/[method].js
+++ pages/api/polly/[method].js
@@ -2,10 +2,52 @@ import pollyABI from '@abi/Polly.json';
 import { ethers } from "ethers";
 import ABIAPI from 'abiapi';
 import { getProvider } from "../../../base/provider";
+import { isArray, isArrayLikeObject, isObject, isObjectLike } from 'lodash';
 
 const abi = new ABIAPI(pollyABI);
 abi.supportedMethods = abi.getReadMethods();
 abi.cacheTTL = 60*60;
+
+function bigNumbersToNumber(value){
+
+    if(value._isBigNumber){
+        return value.toNumber();
+    }
+    else if(isArray(value)){
+        return value.map(bigNumbersToNumber);
+    }
+    else if(isObject(value)){
+
+        for(const key in value) {
+            if (Object.hasOwnProperty.call(value, key)) {
+                value[key] = bigNumbersToNumber(value[key])
+            }
+        }    
+        return value;
+    }
+
+    return value;
+
+}
+
+abi.addGlobalParser(bigNumbersToNumber)
+
+function moduleParser(module){
+
+    return {
+        name: module[0],
+        version: module[1],
+        location: module[2]
+    }
+}
+abi.addParser('getModule', moduleParser)
+abi.addParser('getConfig', function(config){
+    return {
+        name: config[0],
+        owner: config[1],
+        modules: config[2].map(moduleParser)
+    }
+});
 
 export default async (req, res) => {
 
@@ -19,6 +61,7 @@ export default async (req, res) => {
         
         try {
             data.result = await contract[method](...abi.methodParamsFromQuery(method, query));
+            data.result = abi.parse(method, data.result);
         }
         catch(e){
             data.error = e.toString();
